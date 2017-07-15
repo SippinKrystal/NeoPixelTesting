711fd3d31c8aff67f94486760136266bc477daf3
SippinKrystal <SippinKrystal@users.noreply.github.com>
2017-07-14 23:47:36 -0500

    Add files via upload

--- Merica.ino
+++ Merica.ino
@@ -12,7 +12,12 @@
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 //Set the delay variable to one second
-int delayval = 1000; 
+int delayval1 = 1000; 
+int delayval2 = 250;
+int delayval3 = 2000;
+String blue =  "0,0,25";
+String white =  "25,25,25";
+String red = "25,0,0";
 
 void setup() {
 //Start communication with the NeoPixels  
@@ -24,115 +29,255 @@ void loop() {
 int i=0;
 
 //Set pixels of the flag
-//Row 1 of flag
-pixels.setPixelColor(i, pixels.Color(50,50,50));
+//pixel 1 of flag
+pixels.setPixelColor(i, pixels.Color(25,25,25));
+//Send pixel values
+pixels.show();
+//Delay before starting next pixel
+delay(delayval2);
 i++;
-pixels.setPixelColor(i, pixels.Color(0,0,50));
+pixels.setPixelColor(i, pixels.Color(0,0,25));
+//Send pixel values
+pixels.show();
+//Delay before starting next pixel
+delay(delayval2);
 i++;
-pixels.setPixelColor(i, pixels.Color(50,50,50));
+pixels.setPixelColor(i, pixels.Color(25,25,25));
+//Send pixel values
+pixels.show();
+//Delay before starting next pixel
+delay(delayval2);
 i++;
-pixels.setPixelColor(i, pixels.Color(0,0,50));
+pixels.setPixelColor(i, pixels.Color(0,0,25));
+//Send pixel values
+pixels.show();
+//Delay before starting next pixel
+delay(delayval2);
 i++;
-pixels.setPixelColor(i, pixels.Color(50,50,50));
+pixels.setPixelColor(i, pixels.Color(25,25,25));
+//Send pixel values
+pixels.show();
+//Delay before starting next pixel
+delay(delayval2);
 i++;
-pixels.setPixelColor(i, pixels.Color(50,0,0));
+pixels.setPixelColor(i, pixels.Color(25,0,0));
+//Send pixel values
+pixels.show();
+//Delay before starting next pixel
+delay(delayval2);
 i++;
-pixels.setPixelColor(i, pixels.Color(50,0,0));
+pixels.setPixelColor(i, pixels.Color(25,0,0));
 i++;
-pixels.setPixelColor(i, pixels.Color(50,0,0));
+//Send pixel values
+pixels.show();
+//Delay before starting next pixel
+delay(delayval2);
+pixels.setPixelColor(i, pixels.Color(25,0,0));
 i++;
 //Send pixel values
 pixels.show();
 //Delay before starting next row
-delay(delayval);
+delay(delayval1);
 
-//Row 2 of flag
-pixels.setPixelColor(i, pixels.Color(0,0,50));
+//pixel 2 of flag
+pixels.setPixelColor(i, pixels.Color(0,0,25));
+//Send pixel values
+pixels.show();
+//Delay before starting next pixel
+delay(delayval2);
 i++;
-pixels.setPixelColor(i, pixels.Color(50,50,50));
+pixels.setPixelColor(i, pixels.Color(25,25,25));
+//Send pixel values
+pixels.show();
+//Delay before starting next pixel
+delay(delayval2);
 i++;
-pixels.setPixelColor(i, pixels.Color(0,0,50));
+pixels.setPixelColor(i, pixels.Color(0,0,25));
+//Send pixel values
+pixels.show();
+//Delay before starting next pixel
+delay(delayval2);
 i++;
-pixels.setPixelColor(i, pixels.Color(50,50,50));
+pixels.setPixelColor(i, pixels.Color(25,25,25));
+//Send pixel values
+pixels.show();
+//Delay before starting next pixel
+delay(delayval2);
 i++;
-pixels.setPixelColor(i, pixels.Color(0,0,50));
+pixels.setPixelColor(i, pixels.Color(0,0,25));
+//Send pixel values
+pixels.show();
+//Delay before starting next pixel
+delay(delayval2);
 i++;
-pixels.setPixelColor(i, pixels.Color(50,50,50));
+pixels.setPixelColor(i, pixels.Color(25,25,25));
+//Send pixel values
+pixels.show();
+//Delay before starting next pixel
+delay(delayval2);
 i++;
-pixels.setPixelColor(i, pixels.Color(50,50,50));
+pixels.setPixelColor(i, pixels.Color(25,25,25));
+//Send pixel values
+pixels.show();
+//Delay before starting next pixel
+delay(delayval2);
 i++;
-pixels.setPixelColor(i, pixels.Color(50,50,50));
+pixels.setPixelColor(i, pixels.Color(25,25,25));
 i++;
 //Send pixel values
 pixels.show();
 //Delay before starting next row
-delay(delayval);
+delay(delayval1);
 
-//Row 3 of flag
-pixels.setPixelColor(i, pixels.Color(50,50,50));
+//pixel 3 of flag
+pixels.setPixelColor(i, pixels.Color(25,25,25));
+//Send pixel values
+pixels.show();
+//Delay before starting next pixel
+delay(delayval2);
 i++;
-pixels.setPixelColor(i, pixels.Color(0,0,50));
+pixels.setPixelColor(i, pixels.Color(0,0,25));
+//Send pixel values
+pixels.show();
+//Delay before starting next pixel
+delay(delayval2);
 i++;
-pixels.setPixelColor(i, pixels.Color(50,50,50));
+pixels.setPixelColor(i, pixels.Color(25,25,25));
+//Send pixel values
+pixels.show();
+//Delay before starting next pixel
+delay(delayval2);
 i++;
-pixels.setPixelColor(i, pixels.Color(0,0,50));
+pixels.setPixelColor(i, pixels.Color(0,0,25));
+//Send pixel values
+pixels.show();
+//Delay before starting next pixel
+delay(delayval2);
 i++;
-pixels.setPixelColor(i, pixels.Color(50,50,50));
+pixels.setPixelColor(i, pixels.Color(25,25,25));
+//Send pixel values
+pixels.show();
+//Delay before starting next pixel
+delay(delayval2);
 i++;
-pixels.setPixelColor(i, pixels.Color(50,0,0));
+pixels.setPixelColor(i, pixels.Color(25,0,0));
+//Send pixel values
+pixels.show();
+//Delay before starting next pixel
+delay(delayval2);
 i++;
-pixels.setPixelColor(i, pixels.Color(50,0,0));
+pixels.setPixelColor(i, pixels.Color(25,0,0));
+//Send pixel values
+pixels.show();
+//Delay before starting next pixel
+delay(delayval2);
 i++;
-pixels.setPixelColor(i, pixels.Color(50,0,0));
+pixels.setPixelColor(i, pixels.Color(25,0,0));
 i++;
 //Send pixel values
 pixels.show();
 //Delay before starting next row
-delay(delayval);
+delay(delayval1);
 
-//Row 4 of flag
-pixels.setPixelColor(i, pixels.Color(50,50,50));
+//pixel 4 of flag
+pixels.setPixelColor(i, pixels.Color(25,25,25));
+//Send pixel values
+pixels.show();
+//Delay before starting next pixel
+delay(delayval2);
 i++;
-pixels.setPixelColor(i, pixels.Color(50,50,50));
+pixels.setPixelColor(i, pixels.Color(25,25,25));
+//Send pixel values
+pixels.show();
+//Delay before starting next pixel
+delay(delayval2);
 i++;
-pixels.setPixelColor(i, pixels.Color(50,50,50));
+pixels.setPixelColor(i, pixels.Color(25,25,25));
+//Send pixel values
+pixels.show();
+//Delay before starting next pixel
+delay(delayval2);
 i++;
-pixels.setPixelColor(i, pixels.Color(50,50,50));
+pixels.setPixelColor(i, pixels.Color(25,25,25));
+//Send pixel values
+pixels.show();
+//Delay before starting next pixel
+delay(delayval2);
 i++;
-pixels.setPixelColor(i, pixels.Color(50,50,50));
+pixels.setPixelColor(i, pixels.Color(25,25,25));
+//Send pixel values
+pixels.show();
+//Delay before starting next pixel
+delay(delayval2);
 i++;
-pixels.setPixelColor(i, pixels.Color(50,50,50));
+pixels.setPixelColor(i, pixels.Color(25,25,25));
+//Send pixel values
+pixels.show();
+//Delay before starting next pixel
+delay(delayval2);
 i++;
-pixels.setPixelColor(i, pixels.Color(50,50,50));
+pixels.setPixelColor(i, pixels.Color(25,25,25));
+//Send pixel values
+pixels.show();
+//Delay before starting next pixel
+delay(delayval2);
 i++;
-pixels.setPixelColor(i, pixels.Color(50,50,50));
+pixels.setPixelColor(i, pixels.Color(25,25,25));
 i++;
 //Send pixel values
 pixels.show();
 //Delay before starting next row
-delay(delayval);
+delay(delayval1);
 
-//Row 5 of flag
-pixels.setPixelColor(i, pixels.Color(50,0,0));
+//pixel 5 of flag
+pixels.setPixelColor(i, pixels.Color(25,0,0));
+//Send pixel values
+pixels.show();
+//Delay before starting next pixel
+delay(delayval2);
 i++;
-pixels.setPixelColor(i, pixels.Color(50,0,0));
+pixels.setPixelColor(i, pixels.Color(25,0,0));
+//Send pixel values
+pixels.show();
+//Delay before starting next pixel
+delay(delayval2);
 i++;
-pixels.setPixelColor(i, pixels.Color(50,0,0));
+pixels.setPixelColor(i, pixels.Color(25,0,0));
+//Send pixel values
+pixels.show();
+//Delay before starting next pixel
+delay(delayval2);
 i++;
-pixels.setPixelColor(i, pixels.Color(50,0,0));
+pixels.setPixelColor(i, pixels.Color(25,0,0));
+//Send pixel values
+pixels.show();
+//Delay before starting next pixel
+delay(delayval2);
 i++;
-pixels.setPixelColor(i, pixels.Color(50,0,0));
+pixels.setPixelColor(i, pixels.Color(25,0,0));
+//Send pixel values
+pixels.show();
+//Delay before starting next pixel
+delay(delayval2);
 i++;
-pixels.setPixelColor(i, pixels.Color(50,0,0));
+pixels.setPixelColor(i, pixels.Color(25,0,0));
+//Send pixel values
+pixels.show();
+//Delay before starting next pixel
+delay(delayval2);
 i++;
-pixels.setPixelColor(i, pixels.Color(50,0,0));
+pixels.setPixelColor(i, pixels.Color(25,0,0));
+//Send pixel values
+pixels.show();
+//Delay before starting next pixel
+delay(delayval2);
 i++;
-pixels.setPixelColor(i, pixels.Color(50,0,0));
+pixels.setPixelColor(i, pixels.Color(25,0,0));
 i++;
 //Send pixel values
 pixels.show();
-//Delay before starting next row
-delay(delayval);
+//Delay before starting the clear
+delay(delayval3);
 
 //Clear all pixels
 for(int i=0;i<NUMPIXELS;i++){
@@ -141,5 +286,5 @@ for(int i=0;i<NUMPIXELS;i++){
 //Send pixel valus
 pixels.show();
 //Delay before restarting the animation
-delay(delayval);
+delay(delayval1);
 }
